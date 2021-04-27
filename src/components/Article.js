a9c2ded343bf7b9872b48335f37169034ab32c22
Melissa Lawrence <mlawrence537@gmail.com>
2021-04-27 16:36:19 -0400

    Done.

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,11 +4,14 @@ function Article({title, date="January 1, 1970", preview, minutes}) {
     const minFives = Math.ceil(minutes/5)*5;
     const coffNum = minFives / 5
     const coffees = "☕" 
+    const minTens = Math.ceil(minutes/10)*10;
+    const bentoNum = minTens / 10
+    const bento = "🍱"
 
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date} {coffees.repeat(coffNum)} min read</small>
+            <small>{date} {minutes < 30 ? coffees.repeat(coffNum) : bento.repeat(bentoNum)} min read</small>
             <p>{preview}</p>
         </article>
     )
